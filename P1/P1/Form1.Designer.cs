1846e7de6d915ee7115d18f047df2a727fccb5c3
georgerat <ratgeorgeraul2018@gmail.com>
2022-05-04 16:33:40 +0300

    add P1

--- P1/P1/Form1.Designer.cs
+++ P1/P1/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 452);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(623, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aria:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(686, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "aria";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- P1/P1/Form1.cs
+++ P1/P1/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace P1
 {
     public partial class Form1 : Form
     {
+
+        //Se dau n puncte in plan (mici cerculete). Sa se construiasca dreptunghiul de arie minima care contine toate punctele date.
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Green, 3);
+
+            Random r = new Random();
+            int n = r.Next(2, 40); // 2 <= n <= 40
+
+            float xmin = int.MaxValue, ymin = int.MaxValue;
+            float xmax = int.MinValue, ymax = int.MinValue;
+
+            float raza1 = 1;
+            PointF[] m1 = new PointF[n];
+            for (int i = 0; i < n; i++)
+            {
+                m1[i].X = r.Next(50, panel1.Width - 50);
+                m1[i].Y = r.Next(50, panel1.Height - 50);
+                g.DrawEllipse(p, m1[i].X - raza1, m1[i].Y - raza1, raza1 * 2, raza1 * 2);
+
+                if (m1[i].X < xmin)
+                    xmin = m1[i].X;
+                if (m1[i].X > xmax)
+                    xmax = m1[i].X;
+                if (m1[i].Y < ymin)
+                    ymin = m1[i].Y;
+                if (m1[i].Y > ymax)
+                    ymax = m1[i].Y;
+            }
+
+            g.DrawLine(p, xmin, ymin, xmax, ymin);
+            g.DrawLine(p, xmax, ymin, xmax, ymax);
+            g.DrawLine(p, xmax, ymax, xmin, ymax);
+            g.DrawLine(p, xmin, ymax, xmin, ymin);
+
+            float L = xmax - xmin;
+            float l = ymax - ymin;
+            float A = L * l;
+            label1.Text = Convert.ToString(A);
         }
     }
 }
